0dbcc93b2a7d3148d52cf7e22e38ded12176f7b3
arewhyaeenn <arewhyaeenn@gmail.com>
2017-05-24 22:52:08 -0700

    Add problem 1.10

---
+++ Problems/Problem-1.10/extended_euclids.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Ryan McIntyre
+"""
+
+from random import randint
+import matplotlib.pyplot as plt
+
+#-----------------------------------------------------------------------Euclids
+
+def ext_euc(m,n):
+    
+    #check pre-conditions
+    if m<=0 or n<=0 or m!=int(m) or n!=int(n):
+        raise ValueError("Invalid inputs for Euclid's.")
+    
+    #algorithm
+    c = m
+    d = n
+    a = 0
+    x = 1
+    b = 1
+    y = 0
+    while(True):
+        q = int(c/d)#int(float) rounds the float down
+        r = c%d
+        if r == 0:
+            return (a,b)            
+        c = d
+        d = r
+        h = x
+        x = a
+        a = h - q * a
+        h = y
+        y = b
+        b = h - q * b
+        
+#--------------------------------------------------------------------Experiment
+        
+'''
+In order to count the number of "steps" in the algorithm above,
+it is necessary to choose an embed a method of division. It seems
+fitting to use the division defined in Algorithm 1.2. You'll see that
+it has been inserted, along with the variable "steps", which serves as
+a step counter.
+'''
+
+#algorithm, with division and step counter
+def ext_euc_step_count(m,n):
+    
+    if m<=0 or n<=0 or m!=int(m) or n!=int(n):
+        raise ValueError("Invalid inputs for Euclid's.")
+    steps = 4 #4 checks above
+    
+    c = m
+    d = n
+    a = 0
+    x = 1
+    b = 1
+    y = 0
+    steps += 6 #6 assignments before loop
+    while(True):
+        q = 0
+        r = c
+        steps += 4 #2 assignments above, first d<=r and r==0 check below
+        while d <= r:
+            r -= d
+            q += 1
+            steps += 3 #2 assignments, next d<=r check
+        if r == 0:
+            return steps #we only care how many steps were made, in the end
+        c = d
+        d = r
+        h = x
+        x = a
+        a = h - q * a
+        h = y
+        y = b
+        b = h - q * b
+        steps += 12 #8 assignments, 2 multiplications, 2 subtractions
+
+'''
+Next, we gather some data. We'll fix m's size, and vary n<m randomly.
+'''
+
+data = dict()
+size = 1
+while size < 50:
+    sample = 0
+    sample_size = 1000000
+    sample_total = 0
+    while sample < sample_size:
+        m = randint(2**(size-1),(2**size)-1)
+        n = randint(1,m)
+        sample_total += ext_euc_step_count(m,n)
+        sample += 1
+    data[size] = sample_total/sample_size
+    size += 1
+
+'''
+Finally, we'll plot the data. Note that even with very large sample sizes,
+there is a lot of variation. We've saved the results for sample size = 1000000
+'''
+
+#organize data
+X = list(data)
+Y = [data[x] for x in X]
+
+#setup figure
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_xlabel('size of larger input')
+ax.set_ylabel('number of steps')
+fig.suptitle('Complexity of Extended Euclid\'s')
+
+#plot data
+ax.plot(X,Y)
+
+#plot lazy linear guess, from (0,1) to (50,1800)
+ax.plot([4,50],[1,2000],'c--')
+
+#save figure
+fig.savefig('results.png')
